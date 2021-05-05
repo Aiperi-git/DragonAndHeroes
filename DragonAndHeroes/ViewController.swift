7ccc804411af4194f3ef4ea8d7e6ddeb869ee8ca
Aiperi <aiperi.kiyazova@gmail.com>
2021-05-06 01:10:49 +0600

    Initial Commit

---
+++ DragonAndHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragonAndHeroes
+//
+//  Created by Aiperi Kiyazova on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
